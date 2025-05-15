2a638c9c9bdbafef74c7876ee53c68c73354e061
Abdalrhman Abuwarda <114994888+Abdalrhman-Abuwarda@users.noreply.github.com>
2025-05-15 16:02:23 +0300

    refactor user_list_screen final name

--- lib/fetures/Home/uis/blackList_screen.dart
+++ lib/fetures/Home/uis/blackList_screen.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:trustedtallentsvalley/fetures/Home/widgets/usersTable.dart';
+import 'package:trustedtallentsvalley/fetures/Home/widgets/users_list_screen.dart';
 
 import 'trusted_screen.dart';
 
